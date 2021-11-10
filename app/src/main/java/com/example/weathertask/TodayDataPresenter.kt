72f4a3cacbc219c890f66820dc4cbc93504de7d5
DubstCat <voronets.yar@gmail.com>
2021-11-11 00:40:20 +0300

    added: logging intercepror

--- app/src/main/java/com/example/weathertask/TodayDataPresenter.kt
+++ app/src/main/java/com/example/weathertask/TodayDataPresenter.kt
@@ -3,12 +3,15 @@ package com.example.weathertask
 import com.example.weathertask.retrofit.TodaysWeatherJsonResponse
 import com.example.weathertask.retrofit.WeatherApi
 import io.reactivex.rxjava3.core.Observable
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class TodayDataPresenter {
     val retrofit: Retrofit
@@ -18,7 +21,16 @@ class TodayDataPresenter {
     //val todaysWeather:Observable<TodaysWeather>
 
     init {
+        val logger = HttpLoggingInterceptor().also { it.level = HttpLoggingInterceptor.Level.BODY }
+
+        val okHttpClient: OkHttpClient = OkHttpClient().newBuilder()
+            .addInterceptor(logger)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .build()
+
         retrofit = Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl("http://api.openweathermap.org/data/2.5/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
