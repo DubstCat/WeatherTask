35c27ef0945aa3f384439f822453b34684f2d862
DubstCat <voronets.yar@gmail.com>
2021-11-10 19:48:36 +0300

    added navigation

--- app/src/main/java/com/example/weathertask/MainActivity.kt
+++ app/src/main/java/com/example/weathertask/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.weathertask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.weathertask.databinding.ActivityMainBinding
+import com.example.weathertask.ui.ForecastFragment
+import com.example.weathertask.ui.TodayFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val fragmentToday = TodayFragment()
+        val fragmentForecast = ForecastFragment()
+
+        supportFragmentManager.beginTransaction().add(R.id.main_fragment, fragmentToday).commit()
+
+        binding.bottomNavBar.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.today -> {
+                    setCurrentFragment(fragmentToday)
+                }
+                R.id.forecast -> {
+                    setCurrentFragment(fragmentForecast)
+                }
+            }
+            false
+        }
+
     }
+
+    private fun setCurrentFragment(fragment:Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.main_fragment, fragment).commit()
+    }
+
 }
