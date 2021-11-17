ba67934cc478893f7664ce09b538e667f55f7dc5
DubstCat <voronets.yar@gmail.com>
2021-11-17 05:26:43 +0300

    removed unresolved reference

--- app/src/main/java/com/example/weathertask/MainActivity.kt
+++ app/src/main/java/com/example/weathertask/MainActivity.kt
@@ -30,7 +30,7 @@ import java.io.IOException
 import java.util.*
 import android.content.SharedPreferences
 
-r
+
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var checker: Runnable
